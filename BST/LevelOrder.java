c0388b4ea2f3275baf7ac7f7e56b1f32975bda4a
Ayush Rawat <ayushrf@gmail.com>
2026-04-06 08:01:02 +0530

    BST ques

--- BST/LevelOrder.java
+++ BST/LevelOrder.java
@@ -19,19 +19,17 @@ public class LevelOrder {
             if(nodes[i]==-1){
                 return null;
             }
-            // preOrder build...
-            Node newNode = new Node(nodes[i]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
-            return newNode;
+            Node currNode = new Node(nodes[i]);
+            currNode.left = buildTree(nodes);
+            currNode.right = buildTree(nodes);
+            return currNode;
         }
         public void levelOrder(Node rt){
             if(rt==null){
                 return;
             }
-            Queue<Node> q = new LinkedList<>();
+            Queue <Node> q = new LinkedList<>();
             q.add(rt);
-            // for next line
             q.add(null);
             while (!q.isEmpty()) {
                 Node currNode = q.remove();
@@ -44,16 +42,17 @@ public class LevelOrder {
                     }
                 }else{
                     System.out.print(currNode.data+" ");
-                    if (currNode.left!=null) {
+                    if(currNode.left!=null){
                         q.add(currNode.left);
                     }
-                    if (currNode.right!=null) {
+                    if(currNode.right!=null){
                         q.add(currNode.right);
                     }
                 }
             }
         }
     }
+
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree obj = new BinaryTree();

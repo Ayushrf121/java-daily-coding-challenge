7623c24d0988c7511e393035953abc4ff524cfbc
= <=>
2026-07-21 13:00:52 +0530

    feat: Implement Prefix and WordBreak classes for finding unique prefixes and word segmentation

---
+++ TrieDS/Prefix.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prefix {
+    static class Node {
+        Node[] children = new Node[26];
+        int freq;
+
+        Node() {
+            for (int i = 0; i < children.length; i++) {
+                children[i] = null;
+            }
+            this.freq = 1;
+        }
+    }
+
+    static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+    }
+
+    public static String findPrefix(String word) {
+        StringBuilder prefix = new StringBuilder();
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            prefix.append(word.charAt(level));
+            curr = curr.children[idx];
+            if (curr.freq == 1) {
+                return prefix.toString();
+            }
+        }
+        return prefix.toString();
+    }
+
+    public static void main(String[] args) {
+        String arr[] = { "zebra", "dog", "duck", "dove" };
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+        List<String> li = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            li.add(findPrefix(arr[i]));
+        }
+        System.out.println(li);
+    }
+}
---
+++ TrieDS/WordBreak.java
@@ -0,0 +1,71 @@
+// Prefix Problem
+// Find shortest unique prefix for every word in a given list.
+// Assume no word is prefix of another.
+
+// arr[ ] = {"zebra", "dog", "duck", "dove"}
+// ans = {"z", "dog", "du", "dov")
+
+// eVery word in a eiven list.
+
+
+public class WordBreak {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean endOfWords = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        // TC:- O(L) : L is the level or the trie more level of the word more will be
+        // the TC
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.endOfWords = true;
+    }
+
+    public static boolean search(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.endOfWords == true;
+    }
+
+    public static boolean wordBreak(String key) {
+        if(key.length()==0){
+            return true;
+        }
+        for (int i = 1; i <= key.length(); i++) {
+            if (search(key.substring(0, i)) && wordBreak(key.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String arr[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+        String key = "ilikesamsung";
+        System.out.println(wordBreak(key));
+    }
+}

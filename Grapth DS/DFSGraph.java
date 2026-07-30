0deb88b6fb38caaf7d2cfa3f4cdeaf4790b1ad01
= <=>
2026-07-30 12:42:10 +0530

    fix: Correct edge connection in DFSGraph creation

--- Grapth DS/DFSGraph.java
+++ Grapth DS/DFSGraph.java
@@ -17,7 +17,7 @@ public class DFSGraph {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1, 1));
+            
         graph[0].add(new Edge(0, 2, 1));
 
         graph[1].add(new Edge(1, 0, 1));
